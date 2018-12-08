d2257f2a25929c83e7fd806f1010b25808d577ff
David H. Park <dhp56@ilab1.cs.rutgers.edu>
2018-12-08 06:54:15 -0500

    Debugged + Optimized. Client should now be fully functional.

--- banking.h
+++ banking.h
@@ -9,6 +9,10 @@
 #include <dirent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <libgen.h>
+#include <errno.h>
 #include <limits.h>
 #include <fcntl.h>
 #include <pthread.h>
@@ -16,14 +20,27 @@
 #include <netinet/in.h>
 #include <signal.h>
 #include <netdb.h>
+#include <semaphore.h>
 
 
 typedef struct Acct{//typdef would be better here PJ
 	char* name;   //need to dynamically allocate when struct is created.
 	double balance;
-	int session;  // 1: true     0: false
+	int service;  // 1: true     0: false
 }Acct;
 
+typedef struct userinfo{
+	int newsocket;
+	
+}userinfo;
+
+char* trim(char* string);
+
 void* responseOutput(void* arg);
+
+void processInputs(char** cmd);
+
+void processReceipt(char** tokens);
+
 
 #endif
--- bankingClient.c
+++ bankingClient.c
@@ -6,22 +6,110 @@ int clientSocket;
 int status;  //1: active   0: inactive
 
 pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER; //status
+pthread_mutex_t mutex2 = PTHREAD_MUTEX_INITIALIZER; //currentAcct
+
+
+char* trim(char* string){
+	//trim column values
+	//printf("in trim\n");
+	if (strlen(string) == 0 || string == NULL){
+		char* tmp = (char*) malloc(sizeof(char));
+		tmp = "";
+		return tmp;
+	}
+	int bpos = 0;
+	int epos = strlen(string)-1;
+	
+	while(string[bpos] == ' '){
+		bpos++;
+	}
+	
+	while(string[epos] == ' ' || string[epos] == '\n'){
+		epos--;
+	}
+	
+	if (epos < bpos){
+		char* tmp = (char*) malloc(sizeof(char));
+		tmp = "";
+		return tmp;
+	}
+	
+	char* res = (char*)malloc(sizeof(char)*((epos-bpos)+2));
+	int x, y;
+	for (x = bpos, y = 0; x <= epos; x++, y++){
+		res[y] = string[x];
+	}
+	res[y] = '\0';
+	
+	return res;
+}
 
 //Read messages from server and send them to user
 void* responseOutput(void* arg){
 	//This will be a different thread constantly listening to the server
-	char buffer[1024];
+	char buffer[1024] = "";
 	int valread;
+	char** tokens = (char**) malloc(sizeof(char*)*3);
 	
 	while(1){
-      valread = read( clientSocket , buffer, 1024); 
-      printf("%s\n",buffer );
-      
-      if(strcmp(buffer,"terminate")==0)
-		pthread_exit(NULL);
-    }
-}
+		pthread_mutex_lock(&mutex1);
+		if (status == 0){
+			pthread_mutex_unlock(&mutex1);
+			break;
+		}
+		pthread_mutex_unlock(&mutex1);
 
+		tokens[0] = NULL;
+		tokens[1] = NULL;
+		tokens[2] = NULL;
+
+    	valread = recv( clientSocket , buffer, 1024, 0);
+    	if (valread == -1){
+	    	continue;
+    	}
+    	printf("ACK received: %s\n", buffer);
+
+
+    	if(strcmp(buffer, "terminate") == 0){
+    		printf("Server session terminated.\n");
+
+    		pthread_mutex_lock(&mutex1);
+    		status = 0;
+    		pthread_mutex_unlock(&mutex1);
+			pthread_exit(NULL);
+		}
+
+		char* token;
+		char* ptr = buffer;
+		int pos = 0;
+
+		while((token = strtok_r(ptr, ":", &ptr))){
+			tokens[pos] = (char*) malloc(sizeof(char)*((strlen(token))+1));
+			strcpy(tokens[pos], token);
+			pos++;
+		}
+
+		//Process the message.
+		printf("Processing ACK...\n");
+		processReceipt(tokens);
+		printf("Processing Complete\n");
+
+
+		if (tokens[0] != NULL){
+			free(tokens[0]);
+		}
+		if (tokens[1] != NULL){
+			free(tokens[1]);
+		}
+		if (tokens[2] != NULL){
+			free(tokens[2]);
+		}
+    }
+	
+	 free(tokens);
+	 
+    pthread_exit(NULL);
+}
 
 
 int main (int argc, char** argv){
@@ -41,7 +129,7 @@ int main (int argc, char** argv){
 		exit(0);
 	}
 	
-	int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+	int clientSocket = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
 	if (clientSocket < 0){
 		printf("Error in creating socket.\n");
 		exit(0);
@@ -58,305 +146,53 @@ int main (int argc, char** argv){
 		exit(0);	
 	}
 	
-	printf("(1) Attempting to connect...\n");
+	printf("Attempting to connect...\n");
 	while(connect(clientSocket, (struct sockaddr*) &addr, sizeof(addr)) < 0){
-		printf("(1) Retrying...\n");
+		printf("Retrying...\n");
 		sleep(3);
 	}
-	printf("(1) Connection successful.\n");
-	
+	printf("Connection successful.\n");
+
+	status = 1;
 	
 	pthread_t thread;
 	pthread_create(&thread, NULL, responseOutput, NULL);
-
-	active = 1;
 	
 	
 	char buffer[1024] = "";
+	char** cmd = (char**) malloc(sizeof(char*)*2);
 	
-	while(active == 1){
+	while(1){
+		pthread_mutex_lock(&mutex1);
+		if (status == 0){
+			break;
+		}
+		pthread_mutex_unlock(&mutex1);
 
-		char* cmd[2];
+		//char* cmd[2];
 		cmd[0] = NULL;
 		cmd[1] = NULL;
 		
 		char* token;
 		char* ptr;
 		int pos = 0;
+		int ret = 0;
 		
 		printf("Enter command:\n");
-		scanf("%s", buffer);
+		fgets(buffer, 255, stdin);
 		
 		ptr = buffer;
 		while((token = strtok_r(ptr, " ", &ptr))){
-			cmd[pos] = (char*) malloc(sizeof(char)*(strlen(token)+1));
-			strcpy(cmd[pos], token);
+			cmd[pos] = trim(token);
 			pos++;
 		}
+		//printf("cmd[0]: %s\tcmd[1]: %s\n", cmd[0], cmd[1]);
 		
 		//  *NOTE*  TCP message format (create, serve):   "commandNumber:messageLength:message"
 		//  *NOTE*  TCP message format(deposit, withdraw): "commandNumber:messageLength:account,deposit/withdraw amount"
 		//  ACK:   0: error   else ==> success
 
-		char message[1050] = "";
-		char tail[1043] = "";
-
-		if (strcmp(cmd[0], "create") == 0 && cmd[1] != NULL){ 
-			printf("In create\n");
-
-			//create message
-			//TCP MESSAGE FORMAT:  "messageLength:commandNumber:message"
-			strcpy(message, "1:");
-
-			char len[6];			//variable to hold length of message (account)
-			sprintf(len, "%d", strlen(cmd[1]));   //convert length of message into string
-
-			strcat(message, len);
-			strcat(message, ":");
-			strcat(message, cmd[0]);
-			int ret = send(clientSocket, message, 1050, 0);   //might have to cast message to void*
-
-			if (ret == -1){
-				printf("Failed to send message to server (1)\n");
-				continue;
-			}
-
-			//wait for ACK in case account already exists
-			strcpy(buffer, "");
-			printf("Waiting for ACK\n");
-			while(strcmp(buffer, "") == 0){
-				read(clientSocket, buffer, 1024);
-			}
-			printf("ACK received\n");
-
-			if (strcmp(buffer, "0") != 0){
-				printf("Account: %s  created\n", cmd[1]);
-			}else{
-				printf("ERROR: Account %s already exists\n", cmd[1]);
-			}
-			
-		}else if(strcmp(cmd[0], "serve") == 0 && cmd[1] != NULL){
-			printf("In serve\n");
-			
-			//create message
-			//TCP MESSAGE FORMAT:  "messageLength:commandNumber:message"
-			strcpy(message, "2:");
-
-			char len[6];			//variable to hold length of message (account)
-			sprintf(len, "%d", strlen(cmd[1]));   //convert length of message into string
-
-			strcat(message, len);
-			strcat(message, cmd[1]);
-			int ret = send(clientSocket, message, 1050, 0);   //might have to cast message to void*
-
-			if (ret == -1){
-				printf("Failed to send message to server (2)\n");
-				continue;
-			}
-
-			//Wait for ACK in case account doesn't exist
-			strcpy(buffer, "");
-			printf("Waiting for ACK\n");
-			while(strcmp(buffer, "") == 0){
-				read(clientSocket, buffer, 1024);
-			}
-			printf("ACK received\n");
-
-			if (strcmp(buffer, "0") != 0){
-				printf("Account: %s logged in successfully\n", cmd[1]);
-				currentAcct = (char*) malloc(sizeof(char)*(strlen(cmd[1])+1));
-				strcpy(currentAcct, cmd[1]);
-			}else{
-				printf("ERROR: Could not log into account %s\n", cmd[1]);
-			}
-			
-		}else if(strcmp(cmd[0], "deposit") == 0 && cmd[1] != NULL){
-			printf("In deposit\n");
-
-			if (currentAcct == NULL){
-				printf("User is not yet logged in.\n");
-				free(cmd[0]);
-				if (cmd[1] != NULL){
-					free(cmd[1]);
-				}
-				sleep(2);
-				continue;
-			}
-
-			//create message
-			//TCP MESSAGE FORMAT:  "messageLength:commandNumber:account,deposit/withdraw amount"
-			strcpy(messsage, "3:");
-
-			//delimiter betweeen account and deposit/withdraw amount is a comma ','
-			char tmp[1041];
-			strcpy(tmp, currentAcct);
-			strcat(tmp, ",");
-			strcat(tmp, cmd[1]);
-
-			char len[6];
-			sprintf(len, "%d", strlen(tmp));
-			strcat(message, len);
-			strcat(message, tmp);
-			int ret = send(clientSocket, message, 1050, 0);
-
-			if (ret == -1){
-				printf("Failed to send message to server(3)\n");
-				continue;
-			}
-
-			//wait for ACK in case deposit is unsuccessful
-			strcpy(buffer, "");
-			printf("Waiting for ACK\n");
-			while(strcmp(buffer, "") == 0){
-				read(clientSocket, buffer, 1024);
-			}
-			printf("ACK received\n");
-
-			if (strcmp(buffer, "0") != 0){
-				printf("Deposited %s into account %s successfully\n", cmd[1], currentAcct);
-			}else{
-				printf("ERROR: Could not deposit %s into account %s\n", cmd[1], currentAcct);
-			}
-
-		}else if(strcmp(cmd[0], "withdraw") == 0 && cmd[1] != NULL){
-			printf("In withdraw\n");
-
-			if (currentAcct == NULL){
-				printf("User is not yet logged in.\n");
-				free(cmd[0]);
-				if (cmd[1] != NULL){
-					free(cmd[1]);
-				}
-				sleep(2);
-				continue;
-			}
-
-			//create message
-			//TCP MESSAGE FORMAT:  "messageLength:commandNumber:account,deposit/withdraw amount"
-			strcat(tail, "4:");
-
-			char tmp[1041];
-			strcpy(tmp, currentAcct);
-			strcat(tmp, ",");
-			strcat(tmp, cmd[1]);
-
-			char len[6];
-			sprintf(len, "%d", strlen(tmp));
-			strcat(message, len);
-			strcat(message, tmp);
-			int ret = send(clientSocket, message, 1050, 0);
-
-			if (ret == -1){
-				printf("Failed to send message to server (4)\n");
-				continue;
-			}
-
-			//wait for ACK in case deposit is unsuccessful
-			strcpy(buffer, "");
-			printf("Waiting for ACK\n");
-			while(strcmp(buffer, "") == 0){
-				read(clientSocket, buffer, 1024);
-			}
-			printf("ACK received\n");
-
-			if (strcmp(buffer, "0") != 0){
-				printf("Withdrew %s from account %s successfully\n", cmd[1], currentAcct);
-			}else{
-				printf("ERROR: Could not withdraw %s from account %s\n", cmd[1], currentAcct);
-			}
-			
-			
-		}else if(strcmp(cmd[0], "query") == 0){
-			printf("In query\n");
-
-			if (currentAcct == NULL){
-				printf("User is not yet logged in\n");
-				free(cmd[0]);
-				if (cmd[1] != NULL){
-					free(cmd[1]);
-				}
-				sleep(2);
-				continue;
-			}
-
-			strcat(tail, ":5:");
-			strcat(tail, currentAcct);
-
-			char len[6];
-			sprintf(len, "%d", strlen(tail));
-			strcat(message, len);
-			strcat(message, tail);
-			int ret = send(clientSocket, message, 1050, 0);
-
-			if (ret == -1){
-				printf("Could not send to server (5)\n");
-				continue;
-			}
-
-			//wait for ACK from server
-			strcpy(buffer, "");
-			printf("Waiting for ACK\n");
-			while(strcmp(buffer, "") == 0){
-				read(clientSocket, buffer, 1024);
-			}
-			printf("ACK received\n");
-
-			if (strcmp(buffer, "0") != 0){
-				char inputs[2];
-				inputs[0] = NULL;
-				inputs[1] = NULL;
-				int length = 0;
-				char* token;
-				pos = 0;
-				char* ptr = buffer;
-				while((token = strtok_r(ptr, ":", &ptr))){
-					inputs[pos] = (char*) malloc(sizeof(char)*((strlen(token))+1));
-					strcpy(inputs[pos], token);
-				}
-
-				//check integrity of message received.
-				length = atoi(inputs[0]);
-
-				if (inputs[1] == NULL){
-					printf("Error in message received format. Try again.\n");
-					free(inputs[0]);
-				}else if (length != strlen(inputs[1])){
-					printf("Message received is corrupted. Balance received: %s. Try again.\n", inputs[1]);
-					free(inputs[0]);
-					free(inputs[1]);
-				}else{
-					printf("Account: %s\tBalance: %s\n", currentAct, inputs[1]);
-					free(inputs[0]);
-					free(inputs[1]);
-				}
-
-			}else{
-				printf("ERROR: Could not get balance of account %s\n", currentAcct);
-			}
-
-			
-		}else if(strcmp(cmd[0], "end") == 0){
-			printf("In end\n");
-
-			if (currentAcct == NULL){
-				printf("User is not yet logged in\n");
-			}else{
-				printf("Logging out %s", currentAcct);
-				free(currentAcct);
-				currentAcct = NULL;
-			}
-			
-		}else if(strcmp(cmd[0], "quit") == 0){
-			printf("Quitting...\n");
-
-			pthread_mutex_lock(mutex1);
-			status = 0;
-			pthread_mutex_unlock(mutex1);
-
-			break;
-		}else{
-			printf("Invalid command. Try again\n");
-		}
+		processInputs(cmd);
 		
 		if (cmd[0] != NULL){
 			free(cmd[0]);
@@ -368,12 +204,270 @@ int main (int argc, char** argv){
 		sleep(2);
 	}
 	
+	free(cmd);
+	pthread_join(thread, NULL);
+	printf("Disconnected from server\n");
+	
 	if (currentAcct != NULL){
 		free(currentAcct);
 	}
 
 	close(clientSocket);
-	pthread_join(thread);
 	
 	return 0;
+}
+
+//Used to process client input commands
+void processInputs(char** cmd){
+	char message[1050] = "";
+	int ret = 0;
+
+	if (strcmp(cmd[0], "create") == 0 && cmd[1] != NULL){ 
+		//printf("In create\n");
+
+		//Check if user is already logged in
+		pthread_mutex_lock(&mutex2);
+		if (currentAcct != NULL){
+			printf("User %s already logged in. Must be logged out to create\n", currentAcct);
+			pthread_mutex_unlock(&mutex2);
+			return;
+		}
+		pthread_mutex_unlock(&mutex2);
+
+
+		//create message
+		//TCP MESSAGE FORMAT:  "messageLength:commandNumber:message"
+		strcpy(message, "1:");
+
+		char len[6];			//variable to hold length of message (account)
+		sprintf(len, "%d", strlen(cmd[1]));   //convert length of message into string
+
+		strcat(message, len);
+		strcat(message, ":");
+		strcat(message, cmd[0]);
+		ret = send(clientSocket, message, 1050, 0);   //might have to cast message to void*
+			
+	}else if(strcmp(cmd[0], "serve") == 0 && cmd[1] != NULL){
+		//printf("In serve\n");
+
+		//Check if user already logged in.
+		pthread_mutex_lock(&mutex2);
+		if (currentAcct != NULL){
+			printf("User %s already logged in. Must be logged out to create\n", currentAcct);
+			pthread_mutex_unlock(&mutex2);
+			return;
+		}
+		pthread_mutex_unlock(&mutex2);
+			
+		//create message
+		//TCP MESSAGE FORMAT:  "messageLength:commandNumber:message"
+		strcpy(message, "2:");
+
+		char len[6];			//variable to hold length of message (account)
+		sprintf(len, "%d", strlen(cmd[1]));   //convert length of message into string
+
+		strcat(message, len);
+		strcat(message, ":");
+		strcat(message, cmd[1]);
+		ret = send(clientSocket, message, 1050, 0);   //might have to cast message to void*
+			
+	}else if(strcmp(cmd[0], "deposit") == 0 && cmd[1] != NULL){
+		//printf("In deposit\n");
+
+		char tmp[1041];
+		pthread_mutex_lock(&mutex2);
+		if (currentAcct == NULL){
+			pthread_mutex_unlock(&mutex2);
+			printf("User is not yet logged in.\n");
+			return;
+		}
+		strcpy(tmp, currentAcct);
+		pthread_mutex_unlock(&mutex2);
+
+		//create message
+		//TCP MESSAGE FORMAT:  "messageLength:commandNumber:account,deposit/withdraw amount"
+		strcpy(message, "3:");
+
+		//delimiter betweeen account and deposit/withdraw amount is a comma ','
+		strcat(tmp, ",");
+		strcat(tmp, cmd[1]);
+
+		char len[6];
+		sprintf(len, "%d", strlen(tmp));
+		strcat(message, len);
+		strcat(message, ":");
+		strcat(message, tmp);
+		ret = send(clientSocket, message, 1050, 0);     //might have to cast message to void*
+
+	}else if(strcmp(cmd[0], "withdraw") == 0 && cmd[1] != NULL){
+		//printf("In withdraw\n");
+
+		char tmp[1041];
+		pthread_mutex_lock(&mutex2);
+		if (currentAcct == NULL){
+			pthread_mutex_unlock(&mutex2);
+			printf("User is not yet logged in.\n");
+			return;
+		}
+		strcpy(tmp, currentAcct);
+		pthread_mutex_unlock(&mutex2);
+
+		//create message
+		//TCP MESSAGE FORMAT:  "messageLength:commandNumber:account,deposit/withdraw amount"
+		strcat(message, "4:");
+
+		strcat(tmp, ",");
+		strcat(tmp, cmd[1]);
+
+		char len[6];
+		sprintf(len, "%d", strlen(tmp));
+		strcat(message, len);
+		strcat(message, ":");
+		strcat(message, tmp);
+		ret = send(clientSocket, message, 1050, 0);    //might have to cast message to void*	
+			
+	}else if(strcmp(cmd[0], "query") == 0){
+		//printf("In query\n");
+
+		char tmp[1041];
+		pthread_mutex_lock(&mutex2);
+		if (currentAcct == NULL){
+			pthread_mutex_unlock(&mutex2);
+			printf("User is not yet logged in\n");
+			return;
+		}
+		strcpy(tmp, currentAcct);
+		pthread_mutex_unlock(&mutex2);
+
+		strcpy(message, "5:");
+
+		char len[6];
+		sprintf(len, "%d", strlen(tmp));
+		strcat(message, len);
+		strcat(message, ":");
+		strcat(message, tmp);
+		ret = send(clientSocket, message, 1050, 0);
+			
+	}else if(strcmp(cmd[0], "end") == 0){
+		//printf("In end\n");
+		
+		char tmp[1041];
+		pthread_mutex_unlock(&mutex2);
+		if (currentAcct == NULL){
+			pthread_mutex_unlock(&mutex2);
+			printf("User is not yet logged in\n");
+			return;
+		}
+		
+		strcpy(tmp, currentAcct);
+		pthread_mutex_unlock(&mutex2);
+		
+		strcpy(message, "6:");
+		char len[6];
+		sprintf(len, "%d", strlen(tmp));
+		strcat(message, len);
+		strcat(message, ":");
+		strcat(message, tmp);
+		ret = send(clientSocket, message, 1050, 0);		
+			
+	}else if(strcmp(cmd[0], "quit") == 0){
+		printf("Quitting...\n");
+
+		strcpy(message, "7:4:");
+		//Don't need to worry about receiving ACK from server.
+		pthread_mutex_lock(&mutex1);
+		pthread_mutex_lock(&mutex2);
+		status = 0;
+		if (currentAcct != NULL){
+			strcat(message, currentAcct);
+			free(currentAcct);
+		}else{
+			strcat(message, "quit");
+			pthread_mutex_unlock(&mutex2);
+			pthread_mutex_unlock(&mutex1);
+			return;
+		}
+		pthread_mutex_unlock(&mutex2);
+		pthread_mutex_unlock(&mutex1);
+
+		printf("quit: message %s\n", message);
+		return;
+		ret = send(clientSocket, message, 1050, 0);
+
+	}else{
+		printf("Invalid command. Try again\n");
+		return;
+	}
+
+	if (ret == -1 || ret == 0){
+		printf("Failed to send message to server\n");
+	}else{
+		printf("Message sent successfully.\nWaiting for ACK...\n");
+	}
+
+	return;
+}
+
+
+//Used to process the message received from the server
+void processReceipt(char** tokens){
+
+	if (tokens[0] == NULL || tokens[1] == NULL || tokens[2] == NULL){
+		printf("Incorrect message format\n");
+		return;
+	}
+
+	int length = atoi(tokens[1]);
+	if (length != strlen(tokens[2])){
+		printf("ERROR: Message received is corrupted. Message: %s\nMessage length should be: %s\nLength of message received: %s\n", tokens[2], strlen(tokens[2]), tokens[1]);
+		return;
+	}
+
+	if (strcmp(tokens[0], "0") == 0){
+		printf("ERROR: %s\n", tokens[2]);
+		return;
+	}
+
+	char tmpAcct[256];
+	pthread_mutex_lock(&mutex2);
+	if (currentAcct != NULL){
+		strcpy(tmpAcct, currentAcct);
+	}
+	pthread_mutex_unlock(&mutex2);
+
+
+	if (strcmp(tokens[0], "1") == 0){				//ACK for "create"
+		printf("Account: %s  created\n", tokens[2]);
+
+	}else if (strcmp(tokens[0], "2") == 0){			//ACK for "server"
+		pthread_mutex_lock(&mutex2);
+		currentAcct = (char*) malloc(sizeof(char)*(strlen(tokens[2])+1));
+		strcpy(currentAcct, tokens[2]);
+		pthread_mutex_unlock(&mutex2);
+		printf("Logged into account: %s\n", tokens[2]);
+
+	}else if (strcmp(tokens[0], "3") == 0){			//ACK for "deposit"
+		printf("Deposited $%s into account %s\n", tokens[2], tmpAcct);
+
+	}else if (strcmp(tokens[0], "4") == 0){			//ACK for 'withdraw'
+		printf("Withdrew $%s from account %s\n", tokens[2], tmpAcct);
+
+	}else if (strcmp(tokens[0], "5") == 0){			//ACK for "query"
+		printf("Account: %s\tBalance: %s\n", tmpAcct, tokens[2]);
+
+	}else if (strcmp(tokens[0], "6") == 0){			//ACK for "end"
+		pthread_mutex_lock(&mutex2);
+		printf("Logging out %s", currentAcct);
+		free(currentAcct);
+		currentAcct = NULL;
+		pthread_mutex_unlock(&mutex2);
+
+	}else if (strcmp(tokens[0], "7") == 0){
+		printf("Quit client session successfully\n");
+
+	}else{
+		printf("Received invalid message: %s:%s:%s\n", tokens[0], tokens[1], tokens[2]);
+	}
+
+	return;
 }
